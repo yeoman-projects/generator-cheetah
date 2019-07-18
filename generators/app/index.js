64d789db19abc4b0a9b88f73b3ff5b65bbfe8fab
marc <marc.adlington@zebra.com>
2019-07-18 16:12:07 +0100

    scaffolding via yeoman added

---
+++ generators/app/index.js
@@ -0,0 +1,43 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(`Welcome to the Webpack-style-bundler ${chalk.red('generator-webpack-stlye-bundler')} generator!`)
+    );
+
+    this.log(
+      yosay(`if you have any questions, please contact marc.adlington@zebra.com.`)
+    );
+
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'someAnswer',
+        message: 'Would you like to start the installation?',
+        default: true
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      console.log(props);
+      this.props = props;
+    });
+  }
+
+  writing() {
+    this.fs.copy(
+      this.templatePath('./'),
+      this.destinationPath('./')
+    );
+  }
+
+  install() {
+    this.installDependencies();
+  }
+};
---
+++ generators/app/templates/webpack.release.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+module.exports = merge(common, {
+    output: {
+        path: path.join(__dirname, '/release'),
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                MiniCssExtractPlugin.loader, //inject into dom 
+                  'css-loader', //css into javascript
+                  'sass-loader' //scss or sass into css
+                ],
+              },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: 'zebra.css' }),
+        new CleanWebpackPlugin
+    ]
+});
