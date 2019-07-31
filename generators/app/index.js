03e38f3c6020d70b0f856e8c4a79f801cc3150be
marc <marc.adlington@zebra.com>
2019-07-31 11:50:28 +0100

    changes to yeoman interactions with user

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,33 +3,29 @@ const Generator = require('yeoman-generator');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
-  prompting() {
+  async prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(`Welcome to the Cheetah generator!
       If you have any questions, please contact marc.adlington@zebra.com.`)
     );
 
-    const prompts = [
+    this.answers = await this.prompt([
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to start the installation?',
-        default: true
+        type: 'input',
+        name: 'folderName',
+        message: 'What would you like the package name to be?',
+        default: 'Cheetah'
       }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      console.log(props);
-      this.props = props;
-    });
+    ]);
   }
 
   writing() {
+    this.log("prompt", this.answers.folderName);
     this.fs.copy(
       this.templatePath('./**/*'),
-      this.destinationRoot()
+      this.destinationRoot(),
+      {title : this.answers.folderName}
     );
   }
 
